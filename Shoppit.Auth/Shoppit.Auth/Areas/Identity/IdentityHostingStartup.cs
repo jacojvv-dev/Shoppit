e616ff717b1592965ca325b940e4f58ec0ee5a34
Jaco Jansen van Vuuren <jacojvv.hello@gmail.com>
2021-11-14 17:26:48 +0200

    Scaffold identity pages

---
+++ Shoppit.Auth/Shoppit.Auth/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Shoppit.Auth.Data;
+
+[assembly: HostingStartup(typeof(Shoppit.Auth.Areas.Identity.IdentityHostingStartup))]
+namespace Shoppit.Auth.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
